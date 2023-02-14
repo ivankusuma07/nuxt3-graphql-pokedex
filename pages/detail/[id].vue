0b80692b35f73d343e0dc296cd07b2094e798952
ivan.kusuma <ivan.kusuma@privyid.tech>
2023-02-14 14:34:41 +0700

    add navigation guard

--- pages/detail/[id].vue
+++ pages/detail/[id].vue
@@ -5,5 +5,12 @@
 </template>
 
 <script lang="ts" setup>
+ onBeforeRouteLeave ((to, from, next) => {
+  console.log("from",from)
+    if (window?.event?.type === 'popstate' && from.name === 'detail-id')
+      next(false)
+    else
+      next()
+  },)
 const route = useRoute()
 </script>
