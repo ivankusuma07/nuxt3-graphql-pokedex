1c54b4ea946c5638328b6e4bd03b78c8342ff420
ivankusuma07 <ivankusuma07@gmail.com>
2026-06-11 21:07:53 +0700

    chore: npm audit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
+  compatibilityDate: '2026-06-11',
   ssr: false,
+  experimental: {
+    viteEnvironmentApi: true
+  },
   modules: ['@nuxtjs/apollo', '@nuxtjs/tailwindcss'],
   apollo: {
     clients: {
